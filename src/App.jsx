528f0e81561c5a47d18a5f42a07f833c1219faaf
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-23 13:42:11 +0700

    major update dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CardList2 from "./components/section/card/CardList2";
+import CardList from "./components/section/card/CardList";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const [coinsData, setCoinsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("src/api/APICard.json");
+        setCoinsData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
+      {/* <CardList2 coinsData={coinsData} /> */}
       <LandingPage />
+      {/* <CardList coinsData={coinsData} /> */}
     </>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import Navbar from "../components/section/navbar/Navbar";
 import Banner from "../components/section/banner/Banner";
 import ListPost from "../components/section/list post/ListPost";
-
+import { useState, useEffect } from "react";
 import CardList from "../components/section/card/CardList";
-
+import axios from "axios";
 function LandingPage() {
+  const [coinsData, setCoinsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("src/api/APICard.json");
+        setCoinsData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <Navbar />
       <Banner />
-      <ListPost />
-      <CardList />
+      <CardList coinsData={coinsData} />
     </div>
   );
 }
